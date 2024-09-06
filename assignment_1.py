6dcc69d0aa4e3af4a8da77e183faf11741b8a186
Giblert Brandow <simon@0100.se>
2024-09-06 23:46:21 +0200

    Completed task 16, #1

--- assignment_1.py
+++ assignment_1.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Tuple, Callable
 from numpy import *
 import sys
 
@@ -11,9 +11,9 @@ def main() -> None:
     print(f'Task 10b: {task_10(125, 11)}')
 
     if (task_12(x=2.3)):
-        print("The expression is zero when x = 2.3")
+        print('The expression is zero when x = 2.3')
     else:
-        print("The expression is not zero when x = 2.3")
+        print('The expression is not zero when x = 2.3')
 
     print(f'Task 13: {task_13(2.2, 2)}')
 
@@ -26,6 +26,8 @@ def main() -> None:
            i=0
     """
 
+    print(f'Task 16: The root is approximately: {task_16()}')
+
 
 def task_8() -> None:
     sum_1: float = 0.25 + 0.2
@@ -33,10 +35,10 @@ def task_8() -> None:
     x: int = 1
     y: float = 0.5
 
-    print(sum_1)
-    print(sum_2)
-    print(x)
-    print(y)
+    print(f'Sum 1: {sum_1}')
+    print(f'Sum 2: {sum_2}')
+    print(f'Integer: {x}')
+    print(f'Float: {y}')
 
 
 def task_9(x: int, y: int, z: int) -> float:
@@ -70,6 +72,29 @@ def task_13(x: float, k: int) -> float:
         power *= x
     return power
 
+
+def task_16() -> float:
+    def f(x: float) -> float:
+        return 4 * x**3 - x + 2
+
+    return bisection_method(function=f, lower_bound=-3, upper_bound=3)
+
+
+def bisection_method(function: Callable[[float], float], lower_bound: float, upper_bound: float, tolerance: float = 1e-8) -> float:
+    midpoint: float = (lower_bound + upper_bound) / 2
+    f_midpoint: float = function(midpoint)
     
+    while abs(f_midpoint) >= tolerance:
+        midpoint = (lower_bound + upper_bound) / 2
+        f_midpoint = function(midpoint)
+
+        if function(lower_bound) * f_midpoint < 0:
+            upper_bound = midpoint
+        else:
+            lower_bound = midpoint
+
+    return midpoint
+
+
 if __name__ == "__main__":
     main()
