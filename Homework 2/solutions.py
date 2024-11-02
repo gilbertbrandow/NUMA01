296ed4c7aecc1f9e086313ae8816955441631194
Giblert Brandow <simon@0100.se>
2024-11-02 18:21:03 +0100

    Implemented __mul__ (#11)

--- Homework 2/solutions.py
+++ Homework 2/solutions.py
@@ -3,28 +3,58 @@ import numpy as np
 
 
 class Interval:
-    def __init__(self, a: float, b: float) -> None:
+    def __init__(
+        self,
+        a: float,
+        b: float
+    ) -> None:
         self.a = min(a, b)
         self.b = max(a, b)
 
     def __repr__(self) -> str:
         return f"[{self.a}, {self.b}]"
 
-    def __add__(self, other: 'Interval') -> 'Interval':
-         return Interval(self.a + other.a, self.b + other.b)
+    def __add__(
+        self,
+        other: 'Interval'
+    ) -> 'Interval':
+        return Interval(self.a + other.a, self.b + other.b)
 
-    def __sub__(self, other: 'Interval') -> 'Interval':
-         return Interval(self.a - other.b, self.b - other.a)
+    def __sub__(
+        self,
+        other: 'Interval'
+    ) -> 'Interval':
+        return Interval(self.a - other.b, self.b - other.a)
+
+    def __mul__(
+        self,
+        other: 'Interval'
+    ) -> 'Interval':
         
+        products: list = [
+            self.a * other.a,
+            self.a * other.b,
+            self.b * other.a,
+            self.b * other.b
+        ]
+        
+        return Interval(
+            min(products),
+            max(products)
+        )
+
+
 def main() -> None:
     I: Interval = Interval(1, 2)
-    I1: Interval = Interval (1 , 4 )
-    I2: Interval = Interval ( -2 , - 1 )
-    sum: Interval = I1 + I2 
+    I1: Interval = Interval(1, 4)
+    I2: Interval = Interval(-2, - 1)
+    sum: Interval = I1 + I2
     diff: Interval = I1 - I2
+    product: Interval = I1 * I2
     print(sum)
     print(diff)
+    print(product)
 
-    
+
 if __name__ == "__main__":
     main()
