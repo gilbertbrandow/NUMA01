8d8cab6683eec4949bf320f41ed82ff89d297bdc
Giblert Brandow <simon@0100.se>
2024-12-16 22:25:01 +0100

    Minor improvements to wavelet_image class

--- Haar Wavelets/wavelet_image.py
+++ Haar Wavelets/wavelet_image.py
@@ -4,28 +4,36 @@ from PIL import Image
 
 class WaveletImage:
     def __init__(self, filepath: str) -> None:
-        self._array: npt.NDArray = self.convert_image_to_array(filepath)
-        self._array = self.normalize_array_shape()
+        self._image_array: npt.NDArray = self.convert_image_to_array(filepath)
+        self._image_array = self.normalize_array_shape()
 
 
+    @property
+    def image_array(self) -> npt.NDArray:
+        """
+        Returns the underlying image array.
+        """
+        return self._image_array
+    
     def convert_image_to_array(self, filepath: str) -> npt.NDArray:
         image: Image.Image = Image.open(filepath)
         return np.asarray(image)
 
 
     def normalize_array_shape(self) -> npt.NDArray:
-        height: int = self._array.shape[0]
-        width: int = self._array.shape[1]
+        height: int = self._image_array.shape[0]
+        width: int = self._image_array.shape[1]
 
         if height % 2 != 0:
-            self._array = self._array[:-1, :]
+            self._image_array = self._image_array[:-1, :]
             
         if width % 2 != 0:
-            self._array = self._array[:, :-1]
+            self._image_array = self._image_array[:, :-1]
 
-        return self._array
+        return self._image_array
     
     
-    def save_image_to_file(self, filepath: str="./Resources/new_kvinna.jpg") -> None: 
-        newimg: Image.Image = Image.fromarray (self._array)
+    def save_to_file(self, filepath: str="./Resources/new_kvinna.jpg") -> None: 
+        newimg: Image.Image = Image.fromarray (self._image_array)
         newimg.save (filepath)
+        print(f"Image saved to {filepath}")
