3753c1f4df34575a9bca83909855401f46c0cf2f
Giblert Brandow <simon@0100.se>
2025-01-02 22:52:16 +0100

    Updated main

--- Haar Wavelets/main.py
+++ Haar Wavelets/main.py
@@ -1,11 +1,16 @@
-from wavelet_image import WaveletImage
+from wavelet_image import WaveletTransformManager
 
 INPUT_FILEPATH: str = "./Resources/kvinna.jpg"
 OUTPUT_FILEPATH: str = "./Resources/new-kvinna.jpg"
 
 def main() -> None:
-    wavelet_image: WaveletImage = WaveletImage(INPUT_FILEPATH)
-    return None
+    manager: WaveletTransformManager = WaveletTransformManager(INPUT_FILEPATH)
 
+    for _ in range(3):
+        manager.add_compressed_image()
+
+    manager.get_latest_image().apply_inverse_wavelet_transform()
+    manager.get_latest_image().save_image(OUTPUT_FILEPATH)
+    
 if __name__ == "__main__":
     main()
