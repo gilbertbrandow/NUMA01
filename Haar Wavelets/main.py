c01c0c6d74f54ce41e1fcc486713bc1df0c99924
Giblert Brandow <simon@0100.se>
2025-01-02 21:13:16 +0100

    Refactoring

--- Haar Wavelets/main.py
+++ Haar Wavelets/main.py
@@ -5,8 +5,6 @@ OUTPUT_FILEPATH: str = "./Resources/new-kvinna.jpg"
 
 def main() -> None:
     wavelet_image: WaveletImage = WaveletImage(INPUT_FILEPATH)
-    wavelet_image.save_to_file(OUTPUT_FILEPATH)
-    
     return None
 
 if __name__ == "__main__":
