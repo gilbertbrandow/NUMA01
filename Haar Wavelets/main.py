c8d7cd92e93150714fe89c85e915fcc74926a353
Giblert Brandow <simon@0100.se>
2025-01-06 20:24:58 +0100

    (#12)

--- Haar Wavelets/main.py
+++ Haar Wavelets/main.py
@@ -7,11 +7,11 @@ OUTPUT_FILEPATH: str = "./Resources/new-kvinna.jpg"
 def main() -> None:
     
     wi: WaveletImage = WaveletImageIO.from_file(filepath=INPUT_FILEPATH)
-    wi.go_to_iteration(2, matrix_multiplaction=False)
+    wi.go_to_iteration(2, matrix_multiplication=True)
     
     wi.next().next()
     
-    wi.next(matrix_multiplaction=False).prev(matrix_multiplaction=False)
+    wi.next(matrix_multiplication=True).prev(matrix_multiplication=True)
     
     WaveletImageIO.to_file(wavelet_image=wi, filepath=OUTPUT_FILEPATH)
     
