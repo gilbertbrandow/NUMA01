571a80f51e89e3b51255ada1d8fa090542a3c100
Giblert Brandow <simon@0100.se>
2025-01-07 21:22:05 +0100

    Minor refactoring (#12)

--- Haar Wavelets/rgb_wavelet_image.py
+++ Haar Wavelets/rgb_wavelet_image.py
@@ -24,30 +24,46 @@ class AbstractWaveletImage(ABC):
 
 class RGBWaveletImage(AbstractWaveletImage):
     def __init__(self, image_array: npt.NDArray):
-        channels = np.transpose(image_array, (2, 0, 1))
-        self._red, self._green, self._blue = (WaveletImage(channel) for channel in channels)
-    
-    def next(self, matrix_multiplication: bool = True) -> Self:
-        self._red.next(matrix_multiplication=matrix_multiplication)
-        self._green.next(matrix_multiplication=matrix_multiplication)
-        self._blue.next(matrix_multiplication=matrix_multiplication)
-        return self
-    
-    def prev(self, matrix_multiplication: bool = True) -> Self:
-        self._red.prev(matrix_multiplication=matrix_multiplication)
-        self._green.prev(matrix_multiplication=matrix_multiplication)
-        self._blue.prev(matrix_multiplication=matrix_multiplication)
-        return self
-    
-    def go_to_iteration(self, iteration: int, matrix_multiplication: bool = True) -> Self:
-        self._red.go_to_iteration(iteration=iteration, matrix_multiplication=matrix_multiplication)
-        self._green.go_to_iteration(iteration=iteration, matrix_multiplication=matrix_multiplication)
-        self._blue.go_to_iteration(iteration=iteration, matrix_multiplication=matrix_multiplication)
-        return self
-    
+        channels: npt.NDArray = np.transpose(image_array, (2, 0, 1))
+        self._channels: list[WaveletImage] = [
+            WaveletImage(channels[i]) for i in range(channels.shape[0])
+        ]
+
+
+    @property
+    def channels(self) -> list[WaveletImage]:
+        return self._channels
+
+
     @property
     def image_array(self) -> npt.NDArray:
-        r = np.asarray(Image.fromarray(self._red._image_array).convert("L"))
-        g = np.asarray(Image.fromarray(self._green._image_array).convert("L"))
-        b = np.asarray(Image.fromarray(self._blue._image_array).convert("L"))
-        return np.transpose(np.array([r, g, b]), (1, 2, 0))
+        channel_arrays: list[npt.NDArray] = []
+        
+        for wavelet_channel in self._channels:
+            arr: npt.NDArray = np.asarray(
+                Image.fromarray(wavelet_channel._image_array).convert("L")
+            )
+            channel_arrays.append(arr)
+
+        return np.transpose(np.stack(channel_arrays, axis=0), (1, 2, 0))
+    
+    
+    def next(self, matrix_multiplication: bool = True) -> Self:
+        for channel in self._channels:
+            channel.next(matrix_multiplication=matrix_multiplication)
+        return self
+
+
+    def prev(self, matrix_multiplication: bool = True) -> Self:
+        for channel in self._channels:
+            channel.prev(matrix_multiplication=matrix_multiplication)
+        return self
+
+
+    def go_to_iteration(self, iteration: int, matrix_multiplication: bool = True) -> Self:
+        for channel in self._channels:
+            channel.go_to_iteration(
+                iteration=iteration,
+                matrix_multiplication=matrix_multiplication
+            )
+        return self
