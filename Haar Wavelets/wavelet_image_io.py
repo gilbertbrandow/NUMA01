e83776adf972cd8844800123269e517f2ede401f
Giblert Brandow <simon@0100.se>
2025-01-07 21:31:45 +0100

    Fixed I/O file working for both instances (#12)

--- Haar Wavelets/wavelet_image_io.py
+++ Haar Wavelets/wavelet_image_io.py
@@ -1,43 +1,26 @@
 from PIL import Image
 import numpy.typing as npt
 import numpy as np
-#from wavelet_image import WaveletImage
-from rgb_wavelet_image import RGBWaveletImage
+from rgb_wavelet_image import AbstractWaveletImage, RGBWaveletImage
+from wavelet_image import WaveletImage
 
 class WaveletImageIO(object):
     @staticmethod
-    def from_file(filepath: str) -> RGBWaveletImage:
+    def from_file(filepath: str, only_grayscale=True) -> AbstractWaveletImage:
+        if only_grayscale: 
+            image: Image.Image = Image.open(filepath).convert("L")
+            return WaveletImage(np.asarray(image)) 
+        
         image: Image.Image = Image.open(filepath).convert("RGB")
-        array: npt.NDArray = np.asarray(image)
-        return RGBWaveletImage(array)
-
+        return RGBWaveletImage(np.asarray(image)) 
+        
+    
     @staticmethod
-    def to_file(wavelet_image: "RGBWaveletImage", filepath: str, only_compressed: bool = False) -> None:
-        #TODO: Handle cases where only the subarray (upper left) image should be saved
-        #wavelet_image._blue.next().prev()
+    def to_file(wavelet_image: AbstractWaveletImage, filepath: str) -> None:
         img: Image.Image = Image.fromarray(wavelet_image.image_array)
-
+        
+        if isinstance(wavelet_image, WaveletImage):
+            img = img.convert("L")
+            
         img.save(filepath)
         print(f"Saved image to {filepath}")
-
-
-    @staticmethod
-    def from_bytes(data: bytes) -> "WaveletImage":
-        pass
-    
-    
-    @staticmethod 
-    def to_bytes(wavelet_image: "WaveletImage") -> str: 
-        pass
-    
-
-    @staticmethod
-    def save_quadrants(self, wavelet_image: "WaveletImage") -> None:
-        # TODO: The whole purpose of haar wavelet is the ability to send an image in parts, maybe we should implement this functionality?
-        pass
-    
-    
-    @staticmethod
-    def reconstruct_from_quadrants(quadrants: dict[str, npt.NDArray]) -> "WaveletImage":
-        # TODO: Reconstruct WaveletImage from quadrants
-        pass
